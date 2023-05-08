912c1d239c2ab1e8061969f33b93b04f81095582
Anderson Tundisi <astundisi@gmail.com>
2023-05-07 23:09:13 -0300

    Update desafio.kt
    Finalizando o Código

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,38 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String, var formacao: Formacao? = null)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        usuario.formacao = this
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("Anderson")
+    val usuario2 = Usuario("Waleuska")
+    val usuario3 = Usuario("Venilton")
+
+    val conteudo1 = ConteudoEducacional("Kotlin", 120)
+    val conteudo2 = ConteudoEducacional("Java", 180)
+    val conteudo3 = ConteudoEducacional("SQL", 240)
+
+    val formacao1 = Formacao("Boot Camp - DIO TQI Kotlin", listOf(conteudo1, conteudo2, conteudo3))
+    val formacao2 = Formacao("Análise de Dados", listOf(conteudo2))
+    val formacao3 = Formacao("Back End Developer", listOf(conteudo2, conteudo3))
+
+    formacao1.matricular(usuario1)
+    formacao2.matricular(usuario2)
+    formacao3.matricular(usuario3)
+
+    println("${usuario1.nome}, Matriculado em: ${usuario1.formacao?.nome}")
+    println("${usuario2.nome}, Matriculado em: ${usuario2.formacao?.nome}")
+    println("${usuario3.nome}, Matriculado em: ${usuario3.formacao?.nome}")
 }
